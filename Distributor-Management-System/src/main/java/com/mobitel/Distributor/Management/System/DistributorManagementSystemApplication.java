b7145184f63dbb89b0dd0252248c30aebdadfca7
MIHIRIP.OU <mihirie1@gmail.com>
2024-02-09 09:09:36 +0530

    spring start

---
+++ Distributor-Management-System/src/main/java/com/mobitel/Distributor/Management/System/DistributorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mobitel.Distributor.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributorManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributorManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Distributor-Management-System/src/test/java/com/mobitel/Distributor/Management/System/DistributorManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobitel.Distributor.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributorManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
